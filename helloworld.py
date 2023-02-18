0db501598b844113d16068a77137541c79edf1c0
Raj Gopinath <raj@savewithsprout.com>
2023-02-18 08:31:16 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,6 @@
 import sys
 
 def main():
-    print("Hello World! From Python: " + str(sys.version_info))
-    #if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
-    print("No exceptions raised")
+    print("Hello World! From Python: ")
 if __name__ == "__main__":
     main()
